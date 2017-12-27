9b08fbc3d61b401bf2467ba5ee32d8a1a984d09b
李彬瑜 <libinyu@sunlight.bz>
2017-12-27 14:22:47 +0800

    根据 checkstyle 规则调整代码

--- src/main/java/bz/sunlight/mybatis/generator/plugins/BaseMapperGeneratorPlugin.java
+++ src/main/java/bz/sunlight/mybatis/generator/plugins/BaseMapperGeneratorPlugin.java
@@ -11,45 +11,40 @@ import java.util.List;
 
 public class BaseMapperGeneratorPlugin extends PluginAdapter {
 
-    public boolean validate(List<String> warnings) {
-        return true;
+  public boolean validate(List<String> warnings) {
+    return true;
+  }
+
+  /**
+   * 生成dao.
+   */
+  @Override
+  public boolean clientGenerated(Interface interfaze,
+                                 TopLevelClass topLevelClass, IntrospectedTable introspectedTable) {
+    // 主键默认采用java.lang.String
+    FullyQualifiedJavaType fqjt = new FullyQualifiedJavaType("BaseMapper<"
+        + introspectedTable.getBaseRecordType() + ","
+        + introspectedTable.getExampleType() + ","
+        + "java.lang.String" + ">");
+
+    FullyQualifiedJavaType imp = new FullyQualifiedJavaType(
+        "bz.sunlight.BaseMapper");
+
+    // 添加 extends MybatisBaseMapper
+    interfaze.addSuperInterface(fqjt);
+
+    // 添加import bz.sunlight.BaseMapper.
+    interfaze.addImportedType(imp);
+
+    List<Method> methods = interfaze.getMethods();
+    for (Method m : methods) {
+      System.out.println(m.getName());
     }
 
-    /**
-     * 生成dao
-     */
-    @Override
-    public boolean clientGenerated(Interface interfaze,
-                                   TopLevelClass topLevelClass, IntrospectedTable introspectedTable) {
-        /**
-         * 主键默认采用java.lang.String
-         */
-        FullyQualifiedJavaType fqjt = new FullyQualifiedJavaType("BaseMapper<"
-                + introspectedTable.getBaseRecordType() + ","
-                + introspectedTable.getExampleType() + ","
-                + "java.lang.String" + ">");
-        FullyQualifiedJavaType imp = new FullyQualifiedJavaType(
-                "bz.sunlight.BaseMapper");
-        /**
-         * 添加 extends MybatisBaseMapper
-         */
-        interfaze.addSuperInterface(fqjt);
-
-        /**
-         * 添加import bz.sunlight.BaseMapper.
-         */
-        interfaze.addImportedType(imp);
-
-        List<Method> methods = interfaze.getMethods();
-        for (Method m : methods) {
-            System.out.println(m.getName());
-        }
-        /**
-         * 方法不需要
-         */
-        interfaze.getMethods().clear();
-        interfaze.getAnnotations().clear();
-        return true;
-    }
+    // 方法不需要
+    interfaze.getMethods().clear();
+    interfaze.getAnnotations().clear();
+    return true;
+  }
 
 }
